f2ba44c78fc99d50b9328bf7d906563d1444bfe7
bestK1ngArthur <bestK1ngArthur@aol.com>
2016-09-21 14:40:59 +0300

    A bit fixes

---
+++ BMSTU Schedule/BMSTU Library/BMSTULibrary.h
@@ -0,0 +1,13 @@
+//
+//  BMSTULibrary.h
+//  BMSTU Schedule
+//
+//  Created by Artem Belkov on 23/08/16.
+//  Copyright © 2015 Artem Belkov. All rights reserved.
+//
+
+#ifndef BMSTULibrary_h
+#define BMSTULibrary_h
+
+
+#endif /* BMSTULibrary_h */
